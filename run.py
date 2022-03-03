e8954acaeacf475c73ff273c083f0f1516d006e1
Lembox <30897765+Lembox@users.noreply.github.com>
2022-03-03 13:10:31 +0100

    Update run.py

--- run.py
+++ run.py
@@ -6,7 +6,7 @@ import pandas as pd
 
 desired_path = ""
 base_url = "https://airbnb.de"
-search_url = "https://www.airbnb.de/s/Bayreuth/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=april&flexible_trip_dates%5B%5D=march&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&query=Bayreuth&place_id=ChIJGQMtqMWioUcRNOfR0gnfhto&checkin=2022-03-12&checkout=2022-03-19&adults=2&source=structured_search_input_header&search_type=autocomplete_click"
+search_url = "https://www.airbnb.de/s/gießen/homes?tab_id=home_tab&refinement_paths%5B%5D=%2Fhomes&flexible_trip_dates%5B%5D=april&flexible_trip_dates%5B%5D=march&flexible_trip_lengths%5B%5D=weekend_trip&date_picker_type=calendar&adults=2&source=structured_search_input_header&search_type=filter_change"
 
 options = Options()
 options.add_argument('--blink-settings=imagesEnabled=false')
